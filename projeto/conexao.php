86e45267eaca09fcc18ca08e1d5fe77d4e19f90f
Leonardo Figueira Santana <166773262+Leonardo-F-Santana@users.noreply.github.com>
2024-05-29 14:50:16 -0300

    Add files via upload

---
+++ projeto/conexao.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>recebedados</title>
+</head>
+<body>
+
+<?php 
+$conexao = mysqli_connect("localhost", "root", "cliente");
+if (!$conexao){
+    echo "Não conectado";
+}
+else
+    echo "Conectado ao banco";
+$
+
+
+
+?>
+    
+</body>
+</html>
